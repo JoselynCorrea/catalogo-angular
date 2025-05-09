63f694bed7161ac3d5d8f88db457620211465888
JoselynCorrea <159605373+JoselynCorrea@users.noreply.github.com>
2025-05-09 00:31:28 -0600

    changing api url

--- src/app/services/index.service.ts
+++ src/app/services/index.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class IndexService {
 
-  private apiUrl = 'http://127.0.0.1:8000/api/movie'; 
+  private apiUrl = 'https://catalogo-laravel.onrender.com/api/movie'; 
 
   constructor(private http: HttpClient) { }
 
